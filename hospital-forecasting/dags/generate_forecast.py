a4f20cad3d4f8c70b70e5b4d5374d440871aa8c1
RydCri <107228115+RydCri@users.noreply.github.com>
2025-05-06 00:57:22 -0500

    placeholder, dag to generate weekly output from forecast model

---
+++ hospital-forecasting/dags/generate_forecast.py
@@ -0,0 +1,15 @@
+# TODO: task and generate weekly forecast model
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
+# from datetime import datetime
+# from src.model.forecast import generate_and_store_forecast
+#
+# with DAG("generate_forecast",
+#          start_date=datetime(2023, 1, 1),
+#          schedule_interval="@weekly",
+#          catchup=False) as dag:
+#
+#     forecast_task = PythonOperator(
+#         task_id="generate_forecast_task",
+#         python_callable=generate_and_store_forecast,
+#     )
