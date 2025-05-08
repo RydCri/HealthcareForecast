3ec7c27f73f3c02f24597ff755ee79749e508335
RydCri <107228115+RydCri@users.noreply.github.com>
2025-05-08 17:50:30 -0500

    PythonOperator fails import, airflow 2.7+

---
+++ hospital-forecasting/dags/train_forecast_model.py
@@ -0,0 +1,14 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+from src.model.train import train_admissions_model
+
+with DAG("train_forecast_model",
+         start_date=datetime(2023, 1, 1),
+         schedule_interval="@weekly",
+         catchup=False) as dag:
+
+    train_model = PythonOperator(
+        task_id="train_forecast_model_task",
+        python_callable=train_admissions_model,
+    )
