1edf84da1dd10ba44791ad6df6512f448369808d
RydCri <107228115+RydCri@users.noreply.github.com>
2025-05-12 21:32:35 -0500

    airflow reqs linear regression

--- hospital-forecasting/src/model/train.py
+++ hospital-forecasting/src/model/train.py
@@ -40,7 +40,7 @@ def train_model():
     y = df["target"]
 
     # Train/test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=82)
 
     # Train model (Linear Regression here for simplicity)
     model = LinearRegression()
