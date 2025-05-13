a09a40aee480876112cff35a0c086e2f13589b35
RydCri <107228115+RydCri@users.noreply.github.com>
2025-05-12 21:37:43 -0500

    rel imports airflow container

--- hospital-forecasting/dags/ingest_admissions_dag.py
+++ hospital-forecasting/dags/ingest_admissions_dag.py
@@ -1,9 +1,9 @@
+import os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-import os
-from google.cloud import storage
 import pandas as pd
+from google.cloud import storage
 from sqlalchemy import create_engine
 
 # Load env vars from .env if needed
