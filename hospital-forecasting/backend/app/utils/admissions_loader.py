f3bbe4a0a8948907369bb2d367aea5c6330dee58
RydCri <107228115+RydCri@users.noreply.github.com>
2025-06-10 14:05:58 -0500

    testing path on local files

--- hospital-forecasting/backend/app/utils/admissions_loader.py
+++ hospital-forecasting/backend/app/utils/admissions_loader.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import os
 from pathlib import Path
 
-ADMISSIONS_DIR = Path("/opt/airflow/data/admissions")
+# TODO: change back to airflow
+BASE_DIR = Path(__file__).resolve().parents[3]
+ADMISSIONS_DIR = BASE_DIR / "data/hospital"
 
 def load_latest_admissions_data():
-    csvs = sorted(ADMISSIONS_DIR.glob("*admissions_2025.csv"), reverse=True)
+    csvs = sorted(ADMISSIONS_DIR.glob("*admissions.csv"), reverse=True)
     if not csvs:
         raise FileNotFoundError("No admissions CSVs found.")
     return pd.read_csv(csvs[0])
