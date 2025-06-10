461f43cc37856638a0ebbec3a33810617b579171
RydCri <107228115+RydCri@users.noreply.github.com>
2025-06-10 14:06:28 -0500

    set CORS

--- hospital-forecasting/backend/app/__init__.py
+++ hospital-forecasting/backend/app/__init__.py
@@ -1,14 +1,15 @@
-from flask import Flask, CORS
+from flask import Flask
+from flask_cors import CORS
 from .routes.analytics import analytics_bp
 
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(analytics_bp, url_prefix="/api/admissions")
     CORS(app,
          supports_credentials=True,
          resources={
              r"/api/*": {"origins": "http://localhost:5173"},
          }
          )
+    app.register_blueprint(analytics_bp, url_prefix="/api/admissions")
     return app
